088249048afd0f8c26f8f8e4981644655bf91414
Joy Vaz <joy.vaz@uga.edu>
2018-09-25 17:56:50 -0400

    filter sppintrx dataset to include only prots in mammalian carriers, and those that have publications (no associations based on sequence data only), compare and contrast lists of unique prots between datasets

--- data_wrangling.R
+++ data_wrangling.R
@@ -16,14 +16,15 @@ gmpdprot <- read.csv("./Data/GMPD_datafiles/GMPD_main.csv") # Rows are unique ob
 
 # subset datasets to only include protozoa rows and relevant columns
 zooscore %<>% select(parname=ï..ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final, zscore=XC_ZooScore, cscore=XC_CScore, gmpdparname=ParasiteCorrectedName.updated, tm_close=close, tm_nonclose=nonclose, tm_vector=vector, tm_intermediate=intermediate, parphylum=ParPhylum, parclass=ParClass, parorder=ParOrder, parfamily=ParFamily)
-sppintrx %<>% filter(Cargo.classification == "Protozoa") %<>% select(parname=Cargo, carname=Carrier, cartype=Carrier.classification, seqcount=Sequences.count, pubcount=Publications.count)
+sppintrx %<>% filter(Cargo.classification == "Protozoa", Carrier.classification == c("Human", "Mammal", "Domestic", "Primate", "Rodent"), Publications.count >= 1) %<>% select(parname=Cargo, carname=Carrier, cartype=Carrier.classification, seqcount=Sequences.count, pubcount=Publications.count)
 gmpdprot %<>% filter(ParType == "Protozoa", HasBinomialName == "yes") %<>% select(mamtype=Group, carname=HostCorrectedName, carorder=HostOrder, carfamily=HostFamily, carenv=HostEnvironment, gmpdparname=ParasiteCorrectedName, prev=Prevalence, numhosts=HostsSampled, numsamples=NumSamples)
+
 
 # capitalise first letter of binomial names in sppintrx to match zooscore and gmpdprot
 sppintrx$parname <- gsub("(^[a-z])", "\\U\\1", tolower(sppintrx$parname), perl = T)
 sppintrx$carname <- gsub("(^[a-z])", "\\U\\1", tolower(sppintrx$carname), perl = T)
 
-# gmpdparname does not match up with zooscore parname because two binomials have been updated in zooscore 
+# gmpd parname does not match up with zooscore parname because two binomials have been updated in zooscore 
 setdiff(zooscore$parname, gmpdprot$gmpdparname)
 
 # create new parname variable in gmpd 
@@ -33,47 +34,30 @@ gmpdprot %<>% mutate(parname=gmpdparname)
 gmpdprot$parname <- gsub("Cystoisospora canis", "Isospora canis", gmpdprot$parname)
 gmpdprot$parname <- gsub("Plasmodium malariae", "Plasmodium rodhaini", gmpdprot$parname)
 
-# check the parname binomials now match
+# check that the parname binomials now match
 setdiff(zooscore$parname, gmpdprot$parname)
 
-#there are 78 protozoa spp in gmpd that have not been scored in zooscore
-tobescored <- setdiff(gmpdprot$parname, zooscore$parname)
+# there are 78 protozoa spp in gmpd that have not been scored in zooscore
+noscore <- setdiff(gmpdprot$parname, zooscore$parname)
 
 # create tbl listing all unique protozoa spp from all datasets (n = 742)
 parname <- unique(union(sppintrx$parname, gmpdprot$parname))
-allprot <- as.tbl(as.data.frame(parname))
+allprots <- as.tbl(as.data.frame(parname))
 
 # create tbl of all gmpd protozoa-host pairs and combine with zooscore data
 gmpdprotraits <- left_join(gmpdprot, zooscore, by = "parname")
 
 # subset tbl of all gmpd protozoa-host pairs and combine with zooscore data
-gmpdzooscored <- protcartraits %>% filter(zscore >= -1)
+gmpdzooscored <- gmpdprotraits %>% filter(zscore >= -1)
 
-allprotraits <- left_join(allprot, gmpdprotraits)
+protrait <- left_join(allprots, gmpdprotraits)
 
+# subset tbl of all gmpd protozoa-host pairs and combine with zooscore data
+allprotscored <- protrait %>% filter(zscore >= -1)
 
-
-
-
-
-protfreq <- as.data.frame(table(gmpdprot$parname))
-protfreq <- as.tbl(protfreq)
-
-protfre2 <- as.data.frame(table(protcartraits2$parname))
-protfre2 <- as.tbl(protfre2)
-
-protfre3 <- left_join(protfreq, protfre2, by = "Var1")
-
-
-#protcartraits2 <- protcartraits %>% group_by(parname)
-#protraits <- left_join(allprots, zooscore)
-
-
-
-parrange <- protcartraits2 %>% summarise(n_distinct(carname), n_distinct(cartype), sum(seqcount), sum(pubcount))
-parrange %<>% rename(carsppcount=`n_distinct(carname)`, cartypecount=`n_distinct(cartype)`, protseqcount=`sum(seqcount)`, protpubcount=`sum(pubcount)`) 
-protraits <- left_join(protraits, parrange)
-
-
-
-
+# compare and contrast unique prot spp of the two main datasets
+intrxtra <- setdiff(unique(sppintrx$parname), unique(gmpdprot$parname))
+gmpdxtra <- setdiff(unique(gmpdprot$parname), unique(sppintrx$parname))
+sameprot <- intersect(unique(gmpdprot$parname), unique(sppintrx$parname)) # 30 prots shared by both databases, 3 of them in noscore
+# check if zero
+sum(length(intrxtra), length(gmpdxtra), length(sameprot)) - length(allprots$parname)
