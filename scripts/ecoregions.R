4b42b663aa088b06ef8fcc13d91596425798916b
Joy Vaz <joy.vaz@uga.edu>
2019-07-10 18:09:36 -0400

    assign ecoregions to host-parasite pairs

--- scripts/ecoregions.R
+++ scripts/ecoregions.R
@@ -1,13 +1,28 @@
-library(speciesgeocodeR)
+library(sf)
+library(raster)
+library(fasterize)
 
+host_par_points <- as.matrix(gmpdprot[, c(8,7)]) %>% na.omit()
 
-WwfLoad <-
-  function(x){
-    if ( x == ""){x <- getwd()}
-    download.file("http://assets.worldwildlife.org/publications/15/files/original/official_teow.zip",
-                  destfile = file.path(x, "wwf_ecoregions.zip"))#?1349272619")
-    unzip(file.path(x, "wwf_ecoregions.zip"), exdir = file.path(x, "WWF_ecoregions"))
-    file.remove(file.path(x, "wwf_ecoregions.zip"))
-    wwf <- maptools::readShapeSpatial(file.path(x, "WWF_ecoregions", "official", "wwf_terr_ecos.shp"))
-    return(wwf)
-  }
+ecoregions_sf <- st_read("./data/original/WWF_ecoregions_datafiles/wwf_terr_ecos.shp")
+ecoregions_sf$BIOME <- gsub(98, 15, ecoregions_sf$BIOME)
+ecoregions_sf$BIOME <- gsub(99, 16, ecoregions_sf$BIOME)
+ecoregions_sf$BIOME <- as.numeric(ecoregions_sf$BIOME)
+biome_colors <- rev(terrain.colors(18))
+
+st_geometry_type(ecoregions_sf)
+
+r <- raster(ecoregions_sf, res = 1)
+
+ecoregions_all <- fasterize(ecoregions_sf, r, field = "BIOME")
+plot(ecoregions_all, col = biome_colors[3:18])
+points(host_par_points, cex =5)
+plot
+
+ecoregions_biome <- fasterize(ecoregions_sf, r, field = "OBJECTID", by = "BIOME")
+plot(ecoregions_biome)
+
+raster::extract(ecoregions_biome, host_par_points, method='simple', na.rm = TRUE)
+
+x <- raster::extract(ecoregions_biome, host_par_points, method='simple', df = TRUE)
+print(x)
