4711c52abc8bc3ec862ffd5eeeaca7648347d8b0
Joy Vaz <joy.vaz@uga.edu>
2019-08-05 16:24:06 -0400

    update datasets with newly acquired zooscores and 1 added prot species

--- scripts/data_wrangling.R
+++ scripts/data_wrangling.R
@@ -9,97 +9,93 @@ library(dplyr)
 library(stringr)
 
 ## load protozoa zooscores data and subset relevant portions
-#prots178 <- read.csv("./data/original/Zooscore_datafiles/Zooscore_trait_Protozoa.csv") %>% # rows are unique protozoa species (ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final), each is given a zooscore (see Coding Flowchart). Data (unpublished) from: Han lab, Cary Institute of Ecosystem Studies, recieved via email from Barbara Han on 2018.08.06
-#  select(protname=ï..ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final, 
-#         zscore=XC_ZooScore, cscore=XC_CScore, 
-#         gmpdprotname=ParasiteCorrectedName.updated, 
-#         tm_close=close, tm_nonclose=nonclose, tm_vector=vector, tm_intermediate=intermediate, 
-#         parphylum=ParPhylum, parclass=ParClass, parorder=ParOrder, parfamily=ParFamily)
-#  
-#gmpd_tax <- read.csv("./data/original/GMPD_datafiles/GMPD_parasite_taxonomy.csv") %>% # rows are unique parasite species, columns are taxonomic classifications of each species
-#  filter(ParType == "Protozoa", HasBinomialName == "yes") %>% 
-#  select(gmpdprotname=ParasiteCorrectedName, parphylum=ParPhylum, parclass=ParClass, parorder=ParOrder, parfamily=ParFamily) %>% 
-#  distinct()
-#
-#prots048 <- read.csv("./data/modified/48prots.csv") %>% # 48 additional protozoa that have zooscores but no tranmission mode traits recorded in GMPD_parasite_traits.csv
-#  rename(protname=ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final, zscore=XC_ZooScore) %>% 
-#  mutate(cscore=NA, gmpdprotname=protname, tm_close=NA, tm_nonclose=NA, tm_vector=NA, tm_intermediate=NA) %>% 
-#  left_join(gmpd_tax) 
-#
-#protzoos <- rbind(prots178, prots048)
-#
-#write.csv(protzoos, "./data/modified/protzoos.csv")
-#
-#gmpdprot <- read.csv("./data/original/GMPD_datafiles/GMPD_main.csv") %>% # rows are observations of parasite(ParasiteCorrectedName) occurance in a host(HostCorrectedName) for wild primates, carnivores and ungulates. Data from: Stephens et al. 2017, downloaded from https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecy.1799 on 2018.09.11
-#  filter(ParType == "Protozoa", HasBinomialName == "yes", !grepl("no binomial name", HostCorrectedName)) %>% 
-#  select(hosttype=Group, hostname=HostCorrectedName, hostorder=HostOrder, hostfamily=HostFamily, hostenv=HostEnvironment, 
-#         location=LocationName, lat=Latitude, long=Longitude, 
-#         gmpdprotname=ParasiteCorrectedName, 
-#         prev=Prevalence, 
-#         numhosts=HostsSampled, numsamples=NumSamples) %>% 
-#  mutate(ID = seq(len=2484))  # give unique ID to each GMPD record 
-#
-### update gmpdprot with correct prot spp names
-## check  for discrepencies in gmpdprotnames between the two dataframes 
-#
-#setdiff(protzoos$gmpdprotname, gmpdprot$gmpdprotname) # 1 spp in protzoos does is not listed gmpdprot - this is because its host does not have a binomial name and was filtered out above. 
-## remove T. brimonti from protzoos
-#protzoos <- protzoos %>% filter(!grepl("Trypanosoma brimonti", protname))
-#
-## check for discrepencies between final protnames and gmpdprotnames
-#setdiff(protzoos$protname, gmpdprot$gmpdprotname) # the final protnames in protzoos contains 2 corrected protnames that have been updated from the original gmpdprotname
-#
-## create protname variable in gmpd for final protnames
-#gmpdprot <- gmpdprot %>% mutate(protname=gmpdprotname)
-#
-## update the 2 spp names to match zooscore
-#gmpdprot$protname <- gsub("Cystoisospora canis", "Isospora canis", gmpdprot$protname)
-#gmpdprot$protname <- gsub("Plasmodium malariae", "Plasmodium rodhaini", gmpdprot$protname)
-#
-## verify that the protnames in both datasets are now matching
-#setdiff(protzoos$protname, gmpdprot$protname)
-#
-#write.csv(gmpdprot, "./data/modified/gmpdprot.csv")
+prots178 <- read.csv("./data/original/Zooscore_datafiles/Zooscore_trait_Protozoa.csv") %>% # rows are unique protozoa species (ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final), each is given a zooscore (see Coding Flowchart). Data (unpublished) from: Han lab, Cary Institute of Ecosystem Studies, recieved via email from Barbara Han on 2018.08.06
+  select(protname=ï..ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final, 
+         zscore=XC_ZooScore, cscore=XC_CScore, 
+         gmpdprotname=ParasiteCorrectedName.updated, 
+         tm_close=close, tm_nonclose=nonclose, tm_vector=vector, tm_intermediate=intermediate, 
+         parphylum=ParPhylum, parclass=ParClass, parorder=ParOrder, parfamily=ParFamily)
+  
+gmpd_tax <- read.csv("./data/original/GMPD_datafiles/GMPD_parasite_taxonomy.csv") %>% # rows are unique parasite species, columns are taxonomic classifications of each species
+  filter(ParType == "Protozoa", HasBinomialName == "yes") %>% 
+  select(gmpdprotname=ParasiteCorrectedName, parphylum=ParPhylum, parclass=ParClass, parorder=ParOrder, parfamily=ParFamily) %>% 
+  distinct()
+
+prots049 <- read.csv("./data/modified/48prots.csv") %>% # 48 additional protozoa that have zooscores but no tranmission mode traits recorded in GMPD_parasite_traits.csv, plus E. histolytica which got added to this list instead of the original 178.
+  rename(protname=ParasiteCorrectedName_Zooscores_VR_Ver5.0_Final, zscore=XC_ZooScore, cscore=XC_CScore) %>% 
+  mutate(gmpdprotname=protname, tm_close=NA, tm_nonclose=NA, tm_vector=NA, tm_intermediate=NA) %>% 
+  left_join(gmpd_tax) 
+
+protzoos <- rbind(prots178, prots049)
+
+write.csv(protzoos, "./data/modified/protzoos.csv")
+
+gmpdprot <- read.csv("./data/original/GMPD_datafiles/GMPD_main.csv") %>% # rows are observations of parasite(ParasiteCorrectedName) occurance in a host(HostCorrectedName) for wild primates, carnivores and ungulates. Data from: Stephens et al. 2017, downloaded from https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecy.1799 on 2018.09.11
+  filter(ParType == "Protozoa", HasBinomialName == "yes", !grepl("no binomial name", HostCorrectedName)) %>% 
+  select(hosttype=Group, hostname=HostCorrectedName, hostorder=HostOrder, hostfamily=HostFamily, hostenv=HostEnvironment, 
+         location=LocationName, lat=Latitude, long=Longitude, 
+         gmpdprotname=ParasiteCorrectedName, 
+         prev=Prevalence, 
+         numhosts=HostsSampled, numsamples=NumSamples) %>% 
+  mutate(ID = seq(len=2484))  # give unique ID to each GMPD record 
+
+## update gmpdprot with correct prot spp names
+# check  for discrepencies in gmpdprotnames between the two dataframes 
+
+setdiff(protzoos$gmpdprotname, gmpdprot$gmpdprotname) # 1 spp in protzoos does is not listed gmpdprot - this is because its host does not have a binomial name and was filtered out above. 
+# remove T. brimonti from protzoos
+protzoos <- protzoos %>% filter(!grepl("Trypanosoma brimonti", protname))
+
+# check for discrepencies between final protnames and gmpdprotnames
+setdiff(protzoos$protname, gmpdprot$gmpdprotname) # the final protnames in protzoos contains 2 corrected protnames that have been updated from the original gmpdprotname
+
+# create protname variable in gmpd for final protnames
+gmpdprot <- gmpdprot %>% mutate(protname=gmpdprotname)
+
+# update the 2 spp names to match zooscore
+gmpdprot$protname <- gsub("Cystoisospora canis", "Isospora canis", gmpdprot$protname)
+gmpdprot$protname <- gsub("Plasmodium malariae", "Plasmodium rodhaini", gmpdprot$protname)
+
+# verify that the protnames in both datasets are now matching
+setdiff(protzoos$protname, gmpdprot$protname)
+
+write.csv(gmpdprot, "./data/modified/gmpdprot.csv")
 
 #__________________________________________________________________________________
+
+# read in data (remove first row of row numbers)
 
 protzoos <- read.csv("./data/modified/protzoos.csv")[, -1]
 gmpdprot <- read.csv("./data/modified/gmpdprot.csv")[, -1]
 
 # save a list of the prot spp that were not in protzoos
-#noscores <- as.data.frame(setdiff(gmpdprot$protname, protzoos$protname)) %>% rename(protname = `setdiff(gmpdprot$protname, protzoos$protname)`)
-#write.csv(noscores, "./data/modified/noscores.csv")
+noscores <- as.data.frame(setdiff(gmpdprot$protname, protzoos$protname)) %>% rename(protname = `setdiff(gmpdprot$protname, protzoos$protname)`)
+write.csv(noscores, "./data/modified/noscores.csv")
 
 #__________________________________________________________________________________
 
 # add protzoos data to gmpdprot to create protraits
-#protraits <- left_join(gmpdprot, protzoos, by = "protname") %>% 
-#  select(ID, protname, hostname, zscore, cscore, 
-#         tm_close, tm_nonclose, tm_vector, tm_intermediate, 
-#         parphylum, parclass, parorder, parfamily,
-#         hosttype, hostorder, hostfamily, hostenv,
-#         lat, long, location, numhosts, numsamples, prev)
-#write.csv(protraits, "./data/modified/protraits.csv")
+protraits <- left_join(gmpdprot, protzoos, by = "protname") %>% 
+  select(ID, protname, hostname, zscore, cscore, 
+         tm_close, tm_nonclose, tm_vector, tm_intermediate, 
+         parphylum, parclass, parorder, parfamily,
+         hosttype, hostorder, hostfamily, hostenv,
+         lat, long, location, numhosts, numsamples, prev)
+write.csv(protraits, "./data/modified/protraits.csv")
 
 protraits <- read.csv("./data/modified/protraits.csv")[, -1]
 
 # subset tbl of all gmpd protozoa-host pairs and combine with zooscore data
-#protraits_zooscored <- protraits %>% filter(!is.na(zscore))
-#write.csv(protraits_zooscored, "./data/modified/protraits_zooscored.csv")
+protraits_zooscored <- protraits %>% filter(!is.na(zscore))
+write.csv(protraits_zooscored, "./data/modified/protraits_zooscored.csv")
 
 protraits_zooscored <- read.csv("./data/modified/protraits_zooscored.csv")[, 2:24]
 
-# check if protraits_zooscored has 225 prot species
+# check if protraits_zooscored has 226 prot species
 length(unique(protraits_zooscored$protname))
 
 #__________________________________________________________________________________
 
-
-
-# save as csvs
-#write.csv(prots, "./data/modified/prots.csv")
-#write.csv(hosts, "./data/modified/hosts.csv")
-#write.csv(pairs, "./data/modified/pairs.csv")
 
 #__________________________________________________________________________________
 
