7ff30fb6819edcacd8bde6e3313ffe235a6373b1
Joy Vaz <joy.vaz@uga.edu>
2019-08-05 16:25:52 -0400

    identify hosts for each parasite and vice versa

--- scripts/pairsnetwork.R
+++ scripts/pairsnetwork.R
@@ -2,27 +2,32 @@
 ## create affiliate networks for host-prot pairs
 
 # create tbl listing all unique prot spp (n = 255)
-prots <- as.tbl(as.data.frame(unique(gmpdprot$protname))) %>% 
+allprots <- as.tbl(as.data.frame(unique(gmpdprot$protname))) %>% 
   rename(protname = `unique(gmpdprot$protname)`) %>% 
   mutate(prothosts = "empty")
 
 # create tbl listing all unique host spp (n = 251)
-hosts <- as.tbl(as.data.frame(unique(gmpdprot$hostname))) %>% 
+allhosts <- as.tbl(as.data.frame(unique(gmpdprot$hostname))) %>% 
   rename(hostname = `unique(gmpdprot$hostname)`) %>% 
   mutate(hostprots = "empty")
 
 # create tbl listing all unique hp pairs (n = 880)
-pairs <- gmpdprot %>% select(hostname, protname) %>% distinct() %>% as.tbl() %>% 
+allpairs <- gmpdprot %>% select(hostname, protname) %>% distinct() %>% as.tbl() %>% 
   mutate(pairname = paste(protname, ", ", hostname))
 
-for (i in 1:length(prots$protname)) {
-  prots$prothosts[i] <- pairs %>% filter(protname == prots$protname[i]) %>% 
+# save as csvs
+#write.csv(allprots, "./data/modified/allprots.csv")
+#write.csv(allhosts, "./data/modified/allhosts.csv")
+#write.csv(allpairs, "./data/modified/allpairs.csv")
+
+for (i in 1:length(allprots$protname)) {
+  allprots$prothosts[i] <- allpairs %>% filter(protname == allprots$protname[i]) %>% 
     select(hostname) %>% 
     as.vector()
 }
 
-for (i in 1:length(hosts$hostname)) {
-  hosts$hostprots[i] <- pairs %>% filter(hostname == hosts$hostname[i]) %>% 
+for (i in 1:length(allhosts$hostname)) {
+  allhosts$hostprots[i] <- allpairs %>% filter(hostname == allhosts$hostname[i]) %>% 
     select(protname) %>% 
     as.vector()
 }
