371c93e8555ef6d660e835d4f86e2570011b7af4
gititgrrrl <joy.vaz@uga.edu>
2019-08-08 13:07:54 -0400

    create and save tbls listing hosts, prots, and pairs

--- scripts/data_wrangling.R
+++ scripts/data_wrangling.R
@@ -96,6 +96,25 @@ length(unique(protraits_zooscored$protname))
 
 #__________________________________________________________________________________
 
+# create tbl listing all unique prot spp (n = 255)
+#allprots <- as.tbl(as.data.frame(unique(gmpdprot$protname))) %>% 
+#  rename(protname = `unique(gmpdprot$protname)`) %>% 
+#  mutate(prothosts = "empty")
+#
+## create tbl listing all unique host spp (n = 251)
+#allhosts <- as.tbl(as.data.frame(unique(gmpdprot$hostname))) %>% 
+#  rename(hostname = `unique(gmpdprot$hostname)`) %>% 
+#  mutate(hostprots = "empty")
+#
+## create tbl listing all unique hp pairs (n = 880)
+#allpairs <- gmpdprot %>% select(hostname, protname) %>% distinct() %>% as.tbl() %>% 
+#  mutate(pairname = paste(protname, ", ", hostname))
+#
+# save as csvs
+#write.csv(allhosts, "./data/modified/allhosts.csv")
+#write.csv(allprots, "./data/modified/allprots.csv")
+#write.csv(allpairs, "./data/modified/allpairs.csv")
+
 
 #__________________________________________________________________________________
 
