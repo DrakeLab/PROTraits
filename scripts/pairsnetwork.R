5dc60911866d74aa396cec24394854d28d56571e
gititgrrrl <joy.vaz@uga.edu>
2019-08-08 13:08:40 -0400

    calculate features for host-prot affiliate networks

--- scripts/pairsnetwork.R
+++ scripts/pairsnetwork.R
@@ -1,41 +1,45 @@
 ## 7/30/2019
+
 ## create affiliate networks for host-prot pairs
 
-# create tbl listing all unique prot spp (n = 255)
-allprots <- as.tbl(as.data.frame(unique(gmpdprot$protname))) %>% 
-  rename(protname = `unique(gmpdprot$protname)`) %>% 
-  mutate(prothosts = "empty")
-
-# create tbl listing all unique host spp (n = 251)
-allhosts <- as.tbl(as.data.frame(unique(gmpdprot$hostname))) %>% 
-  rename(hostname = `unique(gmpdprot$hostname)`) %>% 
-  mutate(hostprots = "empty")
-
-# create tbl listing all unique hp pairs (n = 880)
-allpairs <- gmpdprot %>% select(hostname, protname) %>% distinct() %>% as.tbl() %>% 
-  mutate(pairname = paste(protname, ", ", hostname))
-
-# save as csvs
-#write.csv(allhosts, "./data/modified/allhosts.csv")
-#write.csv(allprots, "./data/modified/allprots.csv")
-#write.csv(allpairs, "./data/modified/allpairs.csv")
-
-allhosts <- read.csv("./data/modified/allhosts.csv", stringsAsFactors = F)[, -1]
-allprots <- read.csv("./data/modified/allprots.csv", stringsAsFactors = F)[, -1]
 allpairs <- read.csv("./data/modified/allpairs.csv", stringsAsFactors = F)[, -1]
+
+allhosts <- read.csv("./data/modified/allhosts.csv", stringsAsFactors = F)[, -1] %>% 
+  mutate(numprots = NA, protzoos = NA, numprotzoons = NA, propprotzoon = NA, zoonstat = NA)
+
+allprots <- read.csv("./data/modified/allprots.csv", stringsAsFactors = F)[, -1] %>% 
+  left_join(protzoos[,1:2], by = "protname") %>% 
+  mutate(numhosts = NA, numhostprots = NA, hostzoos = NA, numhostzoons = NA, prophostzoon = NA)
+
+protzoos <- read.csv("./data/modified/protzoos.csv", stringsAsFactors = F)[, -1]
+
+for (i in 1:length(allhosts$hostname)) {
+  allhosts$hostprots[i] <- allpairs %>% filter(hostname == allhosts$hostname[i]) %>% 
+    select(protname) %>% 
+    as.vector()
+  allhosts$numprots[i] <- length(allhosts$hostprots[[i]])
+  allhosts$protzoos[i] <- protzoos %>% filter(protname %in% allhosts$hostprots[[i]]) %>% 
+  select(zscore)
+  allhosts$numprotzoons[i] <- length(which(allhosts$protzoos[[i]] >= 1))
+  allhosts$propprotzoon[i] <- allhosts$numprotzoons[i]/allhosts$numprots[i]
+  allhosts$zoonstat[i] <- if(allhosts$numprotzoons[i] > 0){
+    print("1")
+  } else {
+    print("0")
+  }
+}
 
 for (i in 1:length(allprots$protname)) {
   allprots$prothosts[i] <- allpairs %>% filter(protname == allprots$protname[i]) %>% 
     select(hostname) %>% 
     as.vector()
   allprots$prothostprots[i] <- allpairs %>% filter(hostname %in% allprots$prothosts[[i]]) %>% 
-    select(protname) %>% 
+    select(protname) %>% distinct() %>% 
     as.vector()
+  allprots$numhosts[i] <- length(allprots$prothosts[[i]])
+  allprots$numhostprots[i] <- length(allprots$prothostprots[[i]])
+  allprots$hostzoos[i] <- allhosts %>% filter(hostname %in% allprots$prothosts[[i]]) %>% 
+    select(zoonstat)
+  allprots$numhostzoons[i] <- length(which(allprots$hostzoos[[i]] >= 1))
+  allprots$prophostzoon[i] <- allprots$numhostzoons[i]/allprots$numhosts[i]
 }
-
-for (i in 1:length(allhosts$hostname)) {
-  allhosts$hostprots[i] <- allpairs %>% filter(hostname == allhosts$hostname[i]) %>% 
-    select(protname) %>% 
-    as.vector()
-}
-
