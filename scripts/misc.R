e9c5639c225007707d00b90c497c2bb50648bb3f
gititgrrrl <joy.vaz@uga.edu>
2021-03-01 17:07:39 -0500

    save miscellaneous code that could be useful later

--- scripts/misc.R
+++ scripts/misc.R
@@ -66,3 +66,84 @@ names(phylacine)
 
 library(seqinr)
 read.fasta("C:/Users/joych/Dowloads/")
+
+
+# xgboost parameter tuning stuff that i decided not to use --------------------------
+ # based on this article
+
+
+library(xgboost)
+library(ggplot2)
+library(reshape2)
+library(Ecdat)
+
+set.seed(1)
+N = 1000
+k = 10
+x = matrix(rnorm(N*k),N,k)
+b = (-1)^(1:k)
+yaux=(x%*%b)^2
+e = rnorm(N)
+y=yaux+e
+
+# = select train and test indexes = #
+train=sample(1:N,800)
+test=setdiff(1:N,train)
+
+# = parameters = #
+# = eta candidates = #
+eta=c(0.05,0.1,0.2,0.5,1)
+# = colsample_bylevel candidates = #
+cs=c(1/3,2/3,1)
+# = max_depth candidates = #
+md=c(2,4,6,10)
+# = sub_sample candidates = #
+ss=c(0.25,0.5,0.75,1)
+
+# = standard model is the second value  of each vector above = #
+standard=c(2,2,3,2)
+
+# = train and test data = #
+xtrain = sparse_matrix_prot[1:99,] %>% as.matrix()
+ytrain = tmp_prot_Train[1:99]$zoostat
+xtest = sparse_matrix_prot[100:149,] %>% as.matrix()
+ytest = tmp_prot_Train[100:149]$zoostat
+
+set.seed(1)
+conv_eta = matrix(NA,500,length(eta))
+pred_eta = matrix(NA,length(test), length(eta))
+colnames(conv_eta) = colnames(pred_eta) = eta
+for(i in 1:length(eta)){
+  params=list(eta = eta[i], colsample_bylevel=cs[standard[2]],
+              subsample = ss[standard[4]], max_depth = md[standard[3]],
+              min_child_weigth = 1)
+  xgb=xgboost(xtrain, label = ytrain, nrounds = 500, params = params)
+  conv_eta[,i] = xgb$evaluation_log$train_rmse
+  pred_eta[,i] = predict(xgb, xtest)
+}
+
+conv_eta2 = data.frame(iter=1:500, conv_eta)
+conv_eta2.5 <- rbind(conv_eta2, conv_eta2)
+conv_eta3 = melt(conv_eta2.5, id.vars = "iter")
+ggplot(data = conv_eta3) + geom_line(aes(x = iter, y = value, color = variable))
+
+
+(RMSE_eta = sqrt(colMeans((ytest-pred_eta)^2)))
+
+
+set.seed(1)
+conv_cs = matrix(NA,500,length(cs))
+pred_cs = matrix(NA,length(test), length(cs))
+colnames(conv_cs) = colnames(pred_cs) = cs
+for(i in 1:length(cs)){
+  params = list(eta = eta[standard[1]], colsample_bylevel = cs[i],
+                subsample = ss[standard[4]], max_depth = md[standard[3]],
+                min_child_weigth = 1)
+  xgb=xgboost(xtrain, label = ytrain,nrounds = 500, params = params)
+  conv_cs[,i] = xgb$evaluation_log$train_rmse
+  pred_cs[,i] = predict(xgb, xtest)
+}
+
+conv_cs = data.frame(iter=1:500, conv_cs)
+conv_cs = melt(conv_cs, id.vars = "iter")
+ggplot(data = conv_cs) + geom_line(aes(x = iter, y = value, color = variable))
