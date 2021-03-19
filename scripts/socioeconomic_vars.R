cebadda0368f2bbbdc3da3a8118695132ad8307a
gititgrrrl <joy.vaz@uga.edu>
2021-03-19 07:27:39 -0400

    clean up code and extract data from rasters

--- scripts/socioeconomic_vars.R
+++ scripts/socioeconomic_vars.R
@@ -4,19 +4,38 @@ library(ncdf4) # package for netcdf manipulation
 library(raster) # package for raster manipulation
 library(rgdal) # package for geospatial analysis
 library(ggplot2) # package for plotting
+library(magrittr)
+library(tidyverse)
 
+# nc_data <- nc_open("./data/original/Kummu_2018/GDP_per_capita_PPP_1990_2015_v2.nc")
 
-nc_data <- nc_open("./data/original/Kummu_2018/GDP_per_capita_PPP_1990_2015_v2.nc")
+# Import data
 
-GDP_data <- raster("./data/original/Kummu_2018/GDP_per_capita_PPP_1990_2015_v2.nc", layer = 26) # choose 2015 data
+# Choose layer 26 (2015)
 
-GDP_data %>% plot()
+GDP_HDI_2015 <- raster::stack(raster("./data/original/Kummu_2018/GDP_per_capita_PPP_1990_2015_v2.nc", layer = 26),
+                              raster("./data/original/Kummu_2018/HDI_1990_2015_v2.nc", layer = 26))
 
-points <-  gmpdprot %>% dplyr::select(long, lat)
+summary(GDP_HDI_2015)
 
-protdata <- extract(GDP_data, points)
+gmpdprot <- read.csv("./data/modified/gmpd_zooscored.csv") %>% 
+  filter(ParType == "Protozoa") %>% 
+  select(ID = X, protname = gmpdparname, long, lat)
+points <- gmpdprot %>% dplyr::select(long, lat)
+
+protdata <- raster::extract(GDP_HDI_2015, points) %>% as.data.frame()
 protdata %>% head()
 
+prots_GDP_HDI <- cbind(gmpdprot$protname, protdata) 
 
+colnames(prots_GDP_HDI) <- c("protname", "GDP_2015", "HDI_2015")
 
-extract(GDP_data, matrix(c(25.7, -33.48), ncol = 2))
+prots_GDP_HDI_mean <- prots_GDP_HDI %>% group_by(protname) %>% 
+  summarise_if(is.numeric, mean, na.rm = TRUE)
+colnames(prots_GDP_HDI_mean)[2:3] <- paste("mean_", colnames(prots_GDP_HDI_mean)[2:3])
+
+prots_GDP_HDI_mean %>% head()
+
+# coverage
+
+prots_GDP_HDI_mean %>% summarise_all(function(x) mean(!is.na(x))) 
